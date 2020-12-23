ad72686496cf5c62d797c55ffcc526326ac11820
seojinsarejj <s19009@naver.com>
2020-12-24 08:11:18 +0900

    [ADD] Add bottom_up_fibo code

--- practice/dynamic-programming-fibo.py
+++ practice/dynamic-programming-fibo.py
@@ -4,7 +4,7 @@ def fibo(n):
     else:
         return (fibo(n-2) + fibo(n-1))
 
-def memoization_fibo(n):
+def bottom_up_fibo(n):
     memo = []
     for i in range(0,n):
         if i < 2:
@@ -14,7 +14,14 @@ def memoization_fibo(n):
 
     return memo[n-1]
 
+def memoization_fibo(n):
+    if n < 2:
+        return 1
+    if (memo(n)):
+        return memo[n]
+    memo[n] = memoization_fibo(n-1) + memoization_fibo(n-2)
+    return memo[n]
 
-print(fibo(50))
+#print(fibo(50))
 print(memoization_fibo(50))
 
