73906e5f85b4edb9decf6b3d8a89454c50d6aa86
seojinsarejj <s19009@naver.com>
2020-12-27 12:34:45 +0900

    [ADD] Add answer to 체육복.py

--- coding-test/test/체육복.py
+++ coding-test/test/체육복.py
@@ -20,8 +20,10 @@ def solution(n, lost, reserve):
 print(solution(5,[2,4],[1,3,5]))
 print(solution(5,[2,4],[3]))
 '''
-# 2차
 
+
+# 2차 --- 정답
+'''
 def solution(n, lost, reserve):
     
     lost_lst = lost[:]
@@ -42,6 +44,28 @@ def solution(n, lost, reserve):
             reserve.remove(i+1)
 
     return n-len(lost_lst)
+
+print(solution(5,[2,4],[1,3,5]))
+print(solution(5,[2,4],[3]))
+print(solution(5,[1,3,5],[1,2,6]))
+print(solution(5,[1,5,6],[2,6,7]))
+'''
+
+# 3차 --- 수정
+
+def solution(n,lost,reserve):
+
+    _lost = [l for l in lost if l not in reserve]
+    _reserve = [r for r in reserve if r not in lost]
+
+    for i in _reserve:
+        if i-1 in _lost:
+            _lost.remove(i-1)
+        elif i+1 in _lost:
+            _lost.remove(i+1)
+
+
+    return n-len(_lost)
 
 print(solution(5,[2,4],[1,3,5]))
 print(solution(5,[2,4],[3]))
