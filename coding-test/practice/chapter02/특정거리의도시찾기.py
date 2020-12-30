4fff502f8b130ecedae79c426bc0f220b0638503
seojinsarejj <s19009@naver.com>
2020-12-30 17:43:26 +0900

    [CREATE] Create python-regular-expression

--- coding-test/practice/chapter02/특정거리의도시찾기.py
+++ coding-test/practice/chapter02/특정거리의도시찾기.py
@@ -1,12 +1,12 @@
 # [[1,2],[1,3],[2,3],[2,4]]
 # 1차 
 '''
+'''
 from collections import deque
 
 def solution(n,m,road,k,start):
     
     distance = [-1 for _ in range(n+1)]
-    print(distance)
     distance[0] = 0
     distance[start] = 0
     # 0 0 1 1 -1
@@ -29,7 +29,7 @@ def solution(n,m,road,k,start):
     if k not in distance :
         return -1
     
-    result = [i for i in distance if i == k]
+    result = [i for i in range(n+1) if distance[i] == k]
 
     return result
     
@@ -37,4 +37,4 @@ def solution(n,m,road,k,start):
 print(solution(4,4,[[1,2],[1,3],[2,3],[2,4]],2,1))
 print(solution(4,3,[[1,2],[1,3],[1,4]],2,1))
 print(solution(4,4,[[1,2],[1,3],[2,3],[2,4]],1,1))
-'''
+
