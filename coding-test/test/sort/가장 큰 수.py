b0138301c8611016c1cd882e0089ff68a9bf775d
seojinsarejj <s19009@naver.com>
2021-01-02 18:22:48 +0900

    [ADD] Add answer to 가장 큰 수.py

--- coding-test/test/sort/가장 큰 수.py
+++ coding-test/test/sort/가장 큰 수.py
@@ -35,7 +35,45 @@ def solution2(numbers) :
         return answer
             
 
-print(solution2([6,10,2]))
-print(solution2([3, 30, 34, 5, 9]))
-print(solution2([40, 403]))
+# 3차 --- 45.5
+
+def solution3(numbers) :
+        # 6 10 2
+        #[3, 30, 34, 5, 9]
+        result = []
+
+        if max(numbers) == 0 : 
+            return "0"
+
+        for i in range(len(numbers)):
+            num_len = len(str(numbers[i]))
+            if num_len != 1 :
+                value = numbers[i] / (10**(num_len-1))
+                value += value/ 10**num_len
+                value += value/ 10**num_len**2
+            else :
+                value = numbers[i] * 1.1
+            result.append((numbers[i],value))
+
+        result.sort(key=lambda x : x[1],reverse=True)
+
+        answer = "".join(list(map(lambda x : str(x[0]), result)))
+
+        return answer
+
+# 다른 사람의 풀이 --- 정답
+
+def solution(numbers):
+    numbers = sorted(list(map(str, numbers)), key=lambda x: x*5, reverse=True)
+    return str(int(''.join(numbers)))
+
+
+print(solution3([6,10,2]))
+print(solution3([3, 30, 34, 5, 9]))
+print(solution3([40, 403]))
+print(solution3([10, 101]))
+print(solution3([1,11,111,1111]))
+print(solution3([0,0,0,0,0,0]))
+
+# [(403, 4.03), (40, 4.0)]
 
