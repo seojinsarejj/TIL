3b77e38ffbf34629e2d88592c43bfe1a1fcff4bb
seojinsarejj <s19009@naver.com>
2021-01-14 23:27:19 +0900

    [UPDATE] Update 멀쩡한 사각형.py with 다른사람의 풀이

--- coding-test/test/멀쩡한 사각형.py
+++ coding-test/test/멀쩡한 사각형.py
@@ -1,6 +1,6 @@
 # 1차 --- 50.0 / 100/0
 import math
-def solution(w,h):
+def solution1(w,h):
     return w*h - math.ceil(h/w) * w
 
 
@@ -11,3 +11,8 @@ def solution2(w,h):
     return math.ceil(h/w) + h
 
 print(solution2(4,11))
+
+# 다른 사람의 풀이
+import math
+def solution(w,h):
+    return w*h - (w+h-math.gcd(w,h))
