fde7b508fa26214d13b79301e47584916df73b6d
seojinsarejj <s19009@naver.com>
2021-01-15 12:51:18 +0900

    [CREATE] Create 방금 그곡.py

---
+++ coding-test/test/[3차] 방금그곡.py
@@ -0,0 +1,17 @@
+# 1차 --- 40.0 / 100.0
+def solution(m, musicinfos):
+    
+    for music in musicinfos:
+        info = music.split(",")
+        # time = 시간 구하기
+        start,end = info[0].split(":"), info[1].split(":")
+        time = int(end[1]) - int(start[1])
+        during = len("".join(info[3].split("#")))
+        result = info[3] * (time // during) + info[3][:time % during]
+        if m in result :
+            return info[2]
+        
+    return None
+
+
+print(solution("ABCDEFG",["12:00,12:14,HELLO,CDEFGAB", "13:00,13:05,WORLD,ABCDEF"]))
