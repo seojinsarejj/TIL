8696244b92fa04a8474bc65a17d0f6a1bff93536
seojinsarejj <s19009@naver.com>
2021-01-15 14:44:34 +0900

    [CREATE] Create Success

--- coding-test/test/[3차] 방금그곡.py
+++ coding-test/test/[3차] 방금그곡.py
@@ -100,18 +100,52 @@ def solution1(m, musicinfos):
 
 
 # 5차 --- 96.7 / 100.0
+# def rep(m):
+#     m = m.replace("C#","c").replace("D#","d").replace("F#","f").replace("G#","g").replace("A#","a")
+    
+#     return m
+
+# def get_time(start,end):
+#     hour = int(end[0]) - int(start[0])
+#     minit = int(end[1]) - int(start[1])
+#     if minit < 0:
+#         minit = 60 + minit
+
+#     return hour * 60 + minit
+
+# def solution(m, musicinfos):
+    
+#     dic = dict()
+    
+#     for music in musicinfos:
+#         info = music.split(",")
+#         start,end = info[0].split(":"), info[1].split(":")
+#         time = get_time(start,end)
+#         music = rep(info[3])
+#         result = music * (time // len(music)) + music[:time % len(music)]
+#         if rep(m) in result :
+#             if time not in dic.keys() :
+#                 dic[time] = info[2]
+ 
+#     if dic:
+#         return dic[max(dic.keys())]   
+    
+#     return "(None)"
+
+
+
+# 6차 --- 성공...!
+from datetime import datetime
 def rep(m):
-    m = m.replace("C#","c").replace("D#","d").replace("F#","f").replace("G#","g").replace("A#","a")
+    m = m.replace("C#","c").replace("D#","d").replace("F#","f").replace("G#","g").replace("A#","a").replace("E#","e")
     
     return m
 
 def get_time(start,end):
-    hour = int(end[0]) - int(start[0])
-    minit = int(end[1]) - int(start[1])
-    if minit < 0:
-        minit = 60 + minit
+    
+    time = int((datetime.strptime(end, "%H:%M") - datetime.strptime(start, "%H:%M")).total_seconds() / 60)
 
-    return hour * 60 + minit
+    return time
 
 def solution(m, musicinfos):
     
@@ -119,17 +153,17 @@ def solution(m, musicinfos):
     
     for music in musicinfos:
         info = music.split(",")
-        start,end = info[0].split(":"), info[1].split(":")
-        time = get_time(start,end)
+        time = get_time(info[0],info[1])
         music = rep(info[3])
         result = music * (time // len(music)) + music[:time % len(music)]
         if rep(m) in result :
             if time not in dic.keys() :
                 dic[time] = info[2]
- 
+
     if dic:
         return dic[max(dic.keys())]   
     
     return "(None)"
+
 
 print(solution("ABCDEFG",["12:00,12:14,HELLO,CDEFGAB", "13:00,13:05,WORLD,ABCDEF"]))
