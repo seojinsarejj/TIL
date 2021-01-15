65a255ebc565cebc4059aacae8d812806c44dbea
seojinsarejj <s19009@naver.com>
2021-01-15 13:15:45 +0900

    [CREATE] Create 방금그곡.py with 2차

--- coding-test/test/[3차] 방금그곡.py
+++ coding-test/test/[3차] 방금그곡.py
@@ -13,5 +13,25 @@ def solution1(m, musicinfos):
         
     return None
 
+# 2차 --- 56.7 / 100.0
 
-print(solution1("ABCDEFG",["12:00,12:14,HELLO,CDEFGAB", "13:00,13:05,WORLD,ABCDEF"]))
+def rep(m):
+    m = m.replace("C#","c").replace("D#","d").replace("F#","f").replace("G#","g").replace("A#","a")
+    
+    return m
+    
+
+def solution(m, musicinfos):
+    
+    for music in musicinfos:
+        info = music.split(",")
+        start,end = info[0].split(":"), info[1].split(":")
+        time = int(end[1]) - int(start[1])
+        music = rep(info[3])
+        result = music * (time // len(music)) + music[:time % len(music)]
+        if rep(m) in result :
+            return info[2]
+        
+    return None
+
+print(solution2("ABCDEFG",["12:00,12:14,HELLO,CDEFGAB", "13:00,13:05,WORLD,ABCDEF"]))
