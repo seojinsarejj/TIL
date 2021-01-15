0812326ca99f97d67a932e2888cb2b9f1285f17f
seojinsarejj <s19009@naver.com>
2021-01-15 22:15:24 +0900

    [CREATE] Create N개의 최소공배수.py 다른사람의 풀이

--- coding-test/test/practice/N개의 최소공배수.py
+++ coding-test/test/practice/N개의 최소공배수.py
@@ -13,3 +13,14 @@ def solution(arr):
             return i
     
     return sum
+
+# 다른사람의 풀이
+from fractions import gcd
+
+
+def nlcm(num):      
+    answer = num[0]
+    for n in num:
+        answer = n * answer / gcd(n, answer)
+
+    return answer
