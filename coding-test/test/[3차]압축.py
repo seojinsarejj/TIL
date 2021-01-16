e74f00047acee5202679c2b2e07b7cede5d6fae5
seojinsarejj <s19009@naver.com>
2021-01-16 14:45:39 +0900

    [UPDATE] Update [3차]압축 with other_solution

--- coding-test/test/[3차]압축.py
+++ coding-test/test/[3차]압축.py
@@ -34,3 +34,22 @@ def solution(msg):
 
 
 print(solution("KAKAO"))
+
+
+# 다른 사람의 풀이
+
+def other_solution(msg):
+    answer = []
+    tmp = {chr(e + 64): e for e in range(1, 27)}
+    num = 27
+    while msg:
+        tt = 1
+        while msg[:tt] in tmp.keys() and tt <= msg.__len__():
+            tt += 1
+        tt -= 1
+        if msg[:tt] in tmp.keys():
+            answer.append(tmp[msg[:tt]])
+            tmp[msg[:tt + 1]] = num
+            num += 1
+        msg = msg[tt:]
+    return answer
