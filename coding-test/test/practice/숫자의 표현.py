d3b5bdc6fcf4f5962eddf63e2771f5ae92246975
seojinsarejj <s19009@naver.com>
2021-01-19 15:55:05 +0900

    [ADD] Add other_solution to 숫자의 표현.py

--- coding-test/test/practice/숫자의 표현.py
+++ coding-test/test/practice/숫자의 표현.py
@@ -7,3 +7,11 @@ def solution(n):
         if i % 2 != 0 and n % i == 0 or i % 2 == 0 and n % i == n * 0.5:
             count += 1
     return count
+
+# 다른 사람의 풀이
+def expressions(num):
+    result = [i  for i in range(1,num+1,2) if num % i == 0]
+    print(result)
+    return len(result)
+
+print(expressions(10))
