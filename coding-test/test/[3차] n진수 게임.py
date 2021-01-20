8b0d5694bce6b3db2d5f66891bada1056ca9c248
seojinsarejj <s19009@naver.com>
2021-01-20 16:59:26 +0900

    [ADD] Add other_solution to [3차] n진수 게임.py

--- coding-test/test/[3차] n진수 게임.py
+++ coding-test/test/[3차] n진수 게임.py
@@ -25,3 +25,24 @@ def solution(n, t, m, p):
     return answer
 
 
+# 다른 사람의 풀이
+big = ["A","B","C","D","E","F"]
+def other_solution(n, t, m, p):
+    a="0"
+    i=0
+    #for i in range(t*m):
+    while True:
+        if len(a)>=t*m:
+            break
+        b=""
+        j=i
+        while (j):
+            if j%n>9:
+                b=big[j%n-10]+b
+            else:
+                b=str(j%n)+b
+            j=j//n
+        a=a+b
+        i=i+1
+    answer = a[p-1::m][:t]
+    return answer
