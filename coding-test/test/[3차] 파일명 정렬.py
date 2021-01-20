2395bf90d3a2f124b95f69842c45ca3eb17fcbf8
seojinsarejj <s19009@naver.com>
2021-01-20 16:00:21 +0900

    [ADD] Add solution2 to [3차] 파일명정렬.py

--- coding-test/test/[3차] 파일명 정렬.py
+++ coding-test/test/[3차] 파일명 정렬.py
@@ -16,3 +16,22 @@
 #     return files
 
 
+# 2차 --- 성공
+import re
+def solution(files):
+    
+    head = re.compile('\D+')
+    number = re.compile('\d+')
+    
+    for i in range(len(files)):
+        for j in range(len(files)-1-i):
+            
+            check_head = head.findall(files[j])[0].lower() > head.findall(files[j+1])[0].lower()
+            equal_head =  head.findall(files[j])[0].lower() == head.findall(files[j+1])[0].lower()
+            check_number = int(number.findall(files[j])[0].lower()) > int(number.findall(files[j+1])[0].lower())
+            if check_head :
+                files[j],files[j+1] = files[j+1],files[j]
+            elif equal_head and check_number :
+                files[j],files[j+1] = files[j+1],files[j]
+                    
+    return files
