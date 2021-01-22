75058833e4d7da3bb7593dd9dc0d24c08d55007c
seojinsarejj <s19009@naver.com>
2021-01-22 10:01:13 +0900

    [ADD] Add solution3 to 예상 대진표.py

--- coding-test/test/예상 대진표.py
+++ coding-test/test/예상 대진표.py
@@ -26,7 +26,7 @@ def solution2(n,a,b):
     
     lst = [i for i in range(1, n+1)]
     meet = set([a,b])
-    count = 1
+    count = 0
     
     while len(lst) > 2:
         winner = []
@@ -42,3 +42,29 @@ def solution2(n,a,b):
         lst = winner
         
     return count
+
+# 3차 --- 성공
+def solution3(n,a,b):
+    
+    lst = [i for i in range(1, n+1)]
+    meet = set([a,b])
+    count = 0
+    
+    while len(lst) >= 2:
+        winner = []
+        count += 1
+        for i in range(0,len(lst),2):
+            match = lst[i:i+2]
+            if set(match) == meet: 
+                return count 
+            
+            if a in match : winner.append(a)
+            elif b in match : winner.append(b)
+            else: winner.append(lst[i])
+        lst = winner
+        
+    return count
+
+print(solution3(8, 1, 2))
+print(solution3(8, 2, 3))
+print(solution3(8, 4, 7))
