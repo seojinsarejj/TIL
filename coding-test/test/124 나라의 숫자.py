6c8a24bba77c8f0fca81084d163f9a539bf82c1f
seojinsarejj <s19009@naver.com>
2021-01-23 18:35:18 +0900

    [ADD] Add other_solution to 124 나라의 숫자.py

--- coding-test/test/124 나라의 숫자.py
+++ coding-test/test/124 나라의 숫자.py
@@ -35,3 +35,17 @@ def solution2(n):
     return answer[::-1] 
 
 print(solution2(4))
+
+# 다른 사람의 풀이
+def change124(n):
+    num = ['1','2','4']
+    answer = ""
+    
+    while n > 0:
+        n -= 1
+        answer = num[n % 3] + answer
+        n //= 3
+
+    return answer
+
+print(change124(10))
