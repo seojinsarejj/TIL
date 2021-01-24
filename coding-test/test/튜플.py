9e0c6680624381ddb33ad0da49a1b7195a0722a0
seojinsarejj <s19009@naver.com>
2021-01-24 19:08:33 +0900

    [ADD] Add other solution to 튜플.py

--- coding-test/test/튜플.py
+++ coding-test/test/튜플.py
@@ -19,3 +19,11 @@ def solution(s):
     return result
     
 print(solution("{{2},{2,1},{2,1,3},{2,1,3,4}}"))
+
+# 다른 사람의 풀이
+import re
+from collections import Counter
+def other_solution(s):
+    
+    s = Counter(re.findall('\d+', s))
+    return list(map(int, [k for k, v in sorted(s.items(), key=lambda x: x[1], reverse=True)]))
