1116b9a4c167046a147def902ad349f5389ce9ed
seojinsarejj <s19009@naver.com>
2021-01-25 22:06:02 +0900

    [ADD] Add other solution to 행렬의 곱셈.py

--- coding-test/test/practice/행렬의 곱셈.py
+++ coding-test/test/practice/행렬의 곱셈.py
@@ -13,3 +13,7 @@ def solution(arr1, arr2):
         result.append(arr)
         
     return result
+
+# 다른 사람의 풀이
+def productMatrix(A, B):
+    return [[sum(a*b for a, b in zip(A_row,B_col)) for B_col in zip(*B)] for A_row in A]
