feffa4328272eea572899a79f1641170e3c05bb9
seojinsarejj <s19009@naver.com>
2021-01-31 23:26:59 +0900

    [ADD] Add solution2 to 삼각 달팽이.py

--- coding-test/test/삼각 달팽이.py
+++ coding-test/test/삼각 달팽이.py
@@ -1,10 +1,46 @@
 # 1차 --- 33.3 / 100.0
+# def sum_arr(arr):
+#     result = []
+    
+#     for i in arr:
+#         result += i
+    
+#     return result
+
+# def solution(n):
+    
+#     index = 1
+#     arr = [[1 for _ in range(i)] for i in range(1,n+1)]
+#     m = sum([i for i in range(1,n+1)])
+    
+#     for i in range(n):
+        
+#         for j in range(i,n-i):
+#             arr[j][i] = index
+#             index += 1
+#             if index > m :
+#                 return sum_arr(arr)
+                
+#         for j in range(i+1,n-i*2):
+#             arr[n-1-i][j] = index
+#             index += 1
+#             if index > m :
+#                 return sum_arr(arr)
+
+#         for j in range(i+1,n-2-i):
+#             arr[n-1-j][-1*(i+1)] = index
+#             index += 1
+#             if index > m :
+#                 return sum_arr(arr)
+
+
+# 2차 --- 성공
 def sum_arr(arr):
     result = []
     
     for i in arr:
         result += i
-    
+
     return result
 
 def solution(n):
@@ -15,7 +51,7 @@ def solution(n):
     
     for i in range(n):
         
-        for j in range(i,n-i):
+        for j in range(i*2,n-i):
             arr[j][i] = index
             index += 1
             if index > m :
@@ -25,14 +61,15 @@ def solution(n):
             arr[n-1-i][j] = index
             index += 1
             if index > m :
-                return sum_arr(arr)
+                return sum_arr(arr)  
 
-        for j in range(i+1,n-2-i):
+
+        for j in range(i+1,n-1-i*2):
             arr[n-1-j][-1*(i+1)] = index
             index += 1
+            
             if index > m :
                 return sum_arr(arr)
-
 
 
             
