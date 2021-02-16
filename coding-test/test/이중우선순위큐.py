d6a539038b1895a8acb279c6a4141d38881393e7
seojinsarejj <s19009@naver.com>
2021-02-16 12:24:23 +0900

    [ADD] Add solution2 to 이중우선순위큐.py

--- coding-test/test/이중우선순위큐.py
+++ coding-test/test/이중우선순위큐.py
@@ -22,5 +22,40 @@ def solution(operations):
     
     return [0,0]
 
+# 2차 --- 성공
+import heapq
 
+def sort_heap(heap):
+    sort_heap = []
 
+    for i in range(len(heap)):
+        sort_heap.append(heapq.heappop(heap))
+    
+    heap = sort_heap
+
+    return heap
+
+def solution2(operations):
+    
+    heap = []
+    
+    for o in operations:
+
+        operation = o.split(" ")
+        
+        heap = sort_heap(heap)
+
+        if operation[0] == "I":
+            heapq.heappush(heap,int(operation[1]))
+        else :
+            if heap:
+                if operation[1] == "1":
+                    heap = heap[:-1]
+                else :
+                    heapq.heappop(heap)
+                    
+    if heap:
+        heap = sort_heap(heap)
+        return [heap[-1],heap[0]]
+    
+    return [0,0]
